d07becb8177f5ac25e3cd1fdce7f9c63fa12a9f6
souma9830 <prabirkumarshee8@gmail.com>
2026-02-21 21:34:58 +0530

    add reset password route

--- backendTravel/src/routes/auth.routes.js
+++ backendTravel/src/routes/auth.routes.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const { register, login } = require('../Controllers/AuthController');
+const { register, login, sendPasswordResetOtp, resetPassword, forgotOtp } = require('../Controllers/AuthController');
 
 router.post('/register', register);
 router.post('/login', login);
+router.post('/send-reset-otp', sendPasswordResetOtp);
+router.post('/reset-password', resetPassword);
+router.post('/forgot-otp', forgotOtp);
 
 module.exports = router;
