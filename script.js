267938c45bd64e10b1a3df64e381ca3746f7b3eb
GautamM-1 <gautammaity2004@gmail.com>
2026-02-26 15:25:49 +0530

    improve home page ui

--- script.js
+++ script.js
@@ -427,3 +427,24 @@ passwordToggles.forEach(icon => {
         }
     });
 });
+
+/* ===============================
+   SCROLL REVEAL ANIMATION
+================================ */
+
+const revealElements = document.querySelectorAll(".reveal");
+
+function revealOnScroll() {
+    const windowHeight = window.innerHeight;
+
+    revealElements.forEach(el => {
+        const elementTop = el.getBoundingClientRect().top;
+
+        if (elementTop < windowHeight - 100) {
+            el.classList.add("active");
+        }
+    });
+}
+
+window.addEventListener("scroll", revealOnScroll);
+revealOnScroll();
